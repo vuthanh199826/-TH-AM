da955d6a0edd8c724273ab6e6c4f7ebc2dfb4ea5
vuthanh199826 <vuthanh199825@gmail.com>
2021-05-11 13:48:52 +0700

    hi

---
+++ src/mypack/A.java
@@ -0,0 +1,6 @@
+package mypack;
+public class A {
+protected void msg(){
+    System.out.println("hello");
+}
+}
---
+++ src/pack/B.java
@@ -0,0 +1,8 @@
+package pack;
+import mypack.*;
+class B extends A {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.msg();
+    }
+}
